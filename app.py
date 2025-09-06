c5ee30162a048c740f8212617476eaeb8288e851
TaraTony <tonytara567@gmail.com>
2025-09-06 15:43:31 -0700

    added audio player for simplified text from image

--- app.py
+++ app.py
@@ -3,16 +3,21 @@ from flask import Flask, render_template
 from routes.audio import audio_bp
 from routes.image import image_bp
 from flask import Flask, request, jsonify, render_template
+from flask import Flask, send_from_directory
+
 
 
 app = Flask(__name__)
 app.register_blueprint(audio_bp)
-app.register_blueprint(image_bp)
+app.register_blueprint(image_bp , url_prefix='/image')
 
 
 @app.route('/fidget-tools')
 def fidget_tools():
     return render_template('fidget_tools.html')
+@app.route('/uploads/<path:filename>')
+def serve_uploads(filename):
+    return send_from_directory('uploads', filename)
 
 
 if __name__ == '__main__':
--- utils/tts.py
+++ utils/tts.py
@@ -1,17 +1,40 @@
-import pyttsx3
+'''import pyttsx3
+import os
 
 # Create engine once at import time
 engine = pyttsx3.init()
 engine.setProperty('volume', 1.0)
 engine.setProperty('rate', 140)
 
-def speak_text(text, voice_index=0):
+def speak_text_to_file(text, output_file, voice_index=0):
     voices = engine.getProperty('voices')
     if 0 <= voice_index < len(voices):
         engine.setProperty('voice', voices[voice_index].id)
     else:
         print("Invalid voice index. Using default.")
 
-    text = text.replace(".", ".\n").replace(",", ",\n")
-    engine.say(text)
+    # Save the audio to a file
+    engine.save_to_file(text, output_file)
     engine.runAndWait()
+'''
+from gtts import gTTS
+# import pyttsx3
+
+# # Create engine once at import time
+# engine = pyttsx3.init()
+# engine.setProperty('volume', 1.0)
+# engine.setProperty('rate', 140)
+
+# voices = engine.getProperty('voices')
+
+def speak_text_to_file(text, output_file):
+    try:
+        # Create a gTTS object
+        tts = gTTS(text=text, lang='en')
+        
+        # Save the audio as a .wav file
+        tts.save(output_file)
+        print(f"Audio file generated: {output_file}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
